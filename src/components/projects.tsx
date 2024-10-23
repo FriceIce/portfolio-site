d4ed9c476420dfb4cfcbff4148c2a5eef4bf70cd
Isak Tilahun <Tilahun.Isak@gmail.com>
2024-10-23 16:53:43 +0200

    1. Moved all icons from the public folder to a subfolder named 'Icons' for improved file organization
    2. Updated the bg color of project technologies to #f0f0f0

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -30,7 +30,7 @@ const projects = () => {
                       return (
                         <div
                           key={index}
-                          className={`flex items-center bg-neutral-200 py-2 px-3 rounded shadow
+                          className={`flex items-center bg-[#f0f0f0] py-2 px-3 rounded shadow
                         ${
                           tech.name === "MongoDB"
                             ? "gap-0 flex-row-reverse"
